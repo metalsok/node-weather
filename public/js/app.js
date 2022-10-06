1665a65634fb9750fc1e065c1de77a5727e26812
smetallidis <smetallidis@rblogint.net>
2022-10-06 12:44:39 +0200

    implemented very basic fe logic with hbs. Refactored logic in backend

---
+++ public/js/app.js
@@ -0,0 +1,23 @@
+console.log('Hello World');
+
+const form = document.forms[0];
+const locationName = document.getElementById('location');
+const forecast = document.getElementById('forecast');
+
+form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const city = document.getElementById('city').value;
+      if (!city) {
+        forecast.innerText = 'Please insert a valid location!';
+        return;
+      }
+      fetch(`http://localhost:3000/weather?city=${city}`).then(((response) => {
+        if (response) {
+          return response.json().then((data) => {
+            locationName.innerText = data.location;
+            forecast.innerText = data.forecast;
+          });
+        }
+      }));
+    },
+);
--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const weather = require('./apis/weather');
 const express = require('express');
 const yargs = require('yargs');
 const fs = require('fs');
+const path = require('path');
+const hbs = require('hbs');
 
 yargs.command({
   command: 'start',
@@ -34,22 +36,76 @@ yargs.command({
     fs.writeFileSync('api-keys.json', JSON.stringify(keys));
   },
 });
+
 const app = express();
+
+// Setting up paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setting up handlebars engine and path to views
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory
+app.use(express.static(publicDirectoryPath));
+
 const {weatherApiKey, geoApiKey} = JSON.parse(
     fs.readFileSync('api-keys.json'),
     'utf8');
 
-app.get('/geo', (req, res) => {
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather Application',
+    name: 'Sokratis Met.',
+  });
+});
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About Page',
+    name: 'Sokratis Met.',
+  });
+});
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help Page',
+    name: 'Sokratis Met.',
+  });
+});
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: 'Help Page',
+    name: 'Sokratis Met.',
+    errorMessage: 'Help page not found.',
+  });
+});
+
+app.get('/weather', (req, res) => {
   const {city = 'Vienna'} = req.query;
+
   geolocation.geocode(city, geoApiKey, (err, geodata) => {
-    if (geodata) {
-      weather.forecast(geodata, weatherApiKey,
-          (err, weatherMessage) => {
-            weatherMessage ? res.send(weatherMessage) : res.send(err);
-          });
-    } else {
+    if (err) {
       res.send(err);
     }
+    weather.forecast(geodata, weatherApiKey,
+        (err, message) => {
+          if (err) {
+            res.send(err);
+          }
+          res.send({
+            forecast: message, location: geodata.location, address: city,
+          });
+        });
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: 'Error page',
+    errorMessage: 'Page not found',
+    name: 'Sokratis Met.',
   });
 });
 
